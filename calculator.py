f56ffc995ba1dffe749cb8245524bf8dce7326af
egladron <egladron@gmail.com>
2025-11-14 17:25:09 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -4,8 +4,30 @@ calculator.py
 
 One function per operation, in order.
 """
-# First example
+# https://github.com/egladron/lab10-EL-JL
+# Partner 1: Edwin Ladron de Guevara
+# Partner 2: Joshua Lovett
+
+import math
 def add(a, b): 
-    pass
+    return a + b
 
+def subtract(a, b):
+    return a - b
 
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if a == 0:
+        raise ZeroDivisionError
+    return b / a
+
+def logarithm(a, b):
+    try:
+        return math.log(b, a)
+    except ValueError:
+        raise ValueError
+
+def exponent(a, b):
+    return a ** b
